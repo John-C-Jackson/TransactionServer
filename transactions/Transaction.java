9515d4787a8b521aa1e25e96bd572262be1fb21b
cv456 <cv456@nau.edu>
2020-03-26 22:29:18 -0700

    update

--- transactions/Transaction.java
+++ transactions/Transaction.java
@@ -3,37 +3,52 @@ import java.util.ArrayList;
 
 import locks.Lock;
 
-/*
- * Transaction Class
- *  - [] read
- *  - [] write
- *  - [] close
- */
+
 public class Transaction
 {
-    ArrayList<Lock> locks;
-    
-    // not sure if needed?
+    ArrayList<Lock> locks = null;
+    int transactionID;
+
+
+
+    Transaction(int transactionID)
+    {
+      this.transactionID= transactionID;
+      this.locks = new ArrayList();
+    }
+
+
+    public int getID()
+    {
+      return transactionID;
+    }
+
+    // not sure if needed? --- i think it is .....
     public ArrayList<Lock> getLocks()
     {
         return locks;
     }
-    
+
+    public void addLock(Lock lock)
+    {
+      locks.add(lock);
+    }
+
     // temporary skeleton code & stub return
     public int read(int accountId)
     {
         return 0;
     }
-    
+
     // temporary skeleton code & stub return
     public void write(int accountId, int balance)
     {
-        
+
     }
-    
-    // not entirely sure what close is meant to do... 
+
+    // not entirely sure what close is meant to do...
     public void close()
     {
-        
+
     }
 }
--- transactions/TransactionManager.java
+++ transactions/TransactionManager.java
@@ -6,7 +6,7 @@ import java.util.*;
 
 import java.util.ArrayList;
 
-public class TransactionManager // implements MessageTypes
+public class TransactionManager  implements MessageTypes
 {
     /*
      * Transaction Manager Class
@@ -14,22 +14,83 @@ public class TransactionManager // implements MessageTypes
      *  - [] per transaction, open one connection until transaction is closed
      */
 
-    static final ArrayList<Transaction> transactions = new ArrayList<Transaction>();
-    static int transactionCounter = 0;
+    private static final ArrayList<Transaction> transactions = new ArrayList<Transaction>();
+    private static int transactionCounter = 0;
     boolean flag;
 
+    public TransactionManager()
+    {
 
+    }
 
-    /*
-     * read
-     * write
-     * close
-     * open
-    **/
+    public ArrayList<Transaction> getTransactions()
+    {
+      return transactions;
+    }
+
+    public void runTrans(Socket client)
+    {
+      (new TransactionManagerWorker(client).start());
+    }
 
     private class TransactionManagerWorker extends Thread
     {
+      Socket client = null;
+      ObjectInputStream readFromNetwork = null;
+      ObjectInputStream writeToNetwork = null;
 
+      Message message = null;
+
+      Transaction transaction = null;
+      int accountNumber = 0 ;
+      int balance = 0 ;
+
+      boolean continueProcess;
+
+      private TransactionManagerWorker(Socket client)
+      {
+        this.client = client;
+        try
+        {
+          readFromNetwork = new ObjectInputStream(client.getInputStream());
+          writeToNetwork = new ObjectInputStream(client.getInputStream());
+        }
+        catch(IOException ex)
+        {
+          System.exit(1);
+        }
+
+      }
+
+      @Override
+      public void run()
+      {
+        while(continueProcess)
+        {
+          try
+          {
+
+          }
+          catch(IOException e)
+          {
+
+          }
+
+          switch(message.getType())
+          {
+            case OPEN_TRANSACTION:
+              break;
+            case WRITE_REQUEST:
+              break;
+            case READ_TRANSACTION:
+              break;
+            case CLOSE_TRANSACTION:
+              break;
+          }
+
+        }
+
+      }
 
 
     }
