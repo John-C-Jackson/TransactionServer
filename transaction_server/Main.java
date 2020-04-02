be8c0bdfb719a9d18b482381d0ca3b468d9e438b
Aaron <pag87@nau.edu>
2020-04-01 18:57:03 -0700

    added main method to master branch, removed unneccary print statements in PropertyHandler

--- transaction_server/Main.java
+++ transaction_server/Main.java
@@ -7,21 +7,6 @@ public class Main
 {
     public static void main ( String[] args )
     {
-        
-        /*
-         * All of this is temporary code to test PropertyHandler class...
-         */
-        String serverFile = "/Users/aarongiroux/Desktop/serverProps.txt";
-        String clientFile = "/Users/aarongiroux/Desktop/clientProps.txt";
-        
-        Properties serverProps = new PropertyHandler(serverFile);
-        Properties clientProps = new PropertyHandler(clientFile);
-        
-        System.out.println("Host: " + serverProps.getProperty("HOST"));
-        System.out.println("Port: " + serverProps.getProperty("PORT"));
-        System.out.println("Accounts: " + serverProps.getProperty("NUMBER_ACCOUNTS"));
-        System.out.println("Balance: " + serverProps.getProperty("INITIAL_BALANCE"));
-        
-        System.out.println("Transactions: " + clientProps.getProperty("NUMBER_TRANSACTIONS"));
+		(new TransactionServer("config/serverProps.txt")).start();
     }
 }
