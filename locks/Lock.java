0a3c69de938e79123aad19e8b619e30b8e72c42e
Aaron <pag87@nau.edu>
2020-04-01 23:43:59 -0700

    final commit.. still deadlocks but not sure what else to do. Ran through it and deadlocks seem valid

--- locks/Lock.java
+++ locks/Lock.java
@@ -46,16 +46,23 @@ public class Lock implements LockTypes
             trans.addLock(this);
 			trans.log("[Lock.acquire] set " + lockType.typeToString() + " on account number " + account.getAccountNumber());
 		}
+		else if (this.lockType.getType() == NO_LOCK)
+		{
+			this.lockType = lockType;
+			if (!isHolder(trans))
+			{
+				holders.add(trans);
+			}
+			trans.log("[Lock.acquire] set " + lockType.typeToString() + " on account number " + account.getAccountNumber());
+		}
         // if another transaction has a read lock, share it
-        else if(this.lockType.getType() == READ_LOCK && !isHolder(trans))
+        else if(!holders.isEmpty() && this.lockType.getType() == READ_LOCK && lockType.getType() == READ_LOCK && !holders.contains(trans))
         {
-            holders.add(trans);
-            trans.addLock(this);
+			holders.add(trans);
 			trans.log("[Lock.acquire] sharing " + lockType.typeToString() + " on account number " + account.getAccountNumber());
         }
         // if this transaction is a holder but needs a more exclusive lock
-        // TO-DO: This probably needs a stronger condition
-        else if (holders.size() == 1 && this.lockType.getType() == READ_LOCK && lockType.getType() == WRITE_LOCK)
+        else if (holders.contains(trans) && this.lockType.getType() == READ_LOCK && lockType.getType() == WRITE_LOCK)
         {
 			trans.log("[Lock.acquire] promote " + this.lockType.typeToString() + " to " + lockType.typeToString() + " on account " + account.getAccountNumber());
             this.lockType.promote();
@@ -71,11 +78,8 @@ public class Lock implements LockTypes
         holders.remove(trans);
 		trans.log("[Lock.release] releasing lock on account " + account.getAccountNumber());
 
-        if (holders.isEmpty())
-        {
-            lockType.setNone();
-			trans.log("[Lock.release] account " + account.getAccountNumber() + " now has no locks");
-        }
+        lockType.setNone();
+		trans.log("[Lock.release] account " + account.getAccountNumber() + " now has no locks");
 
         notifyAll();
     }
@@ -83,6 +87,10 @@ public class Lock implements LockTypes
     // TO-DO: implement this function
     public boolean isConflict(Transaction trans, LockType type)
     {
+		if (type.getType() == NO_LOCK)
+		{
+			return false;
+		}
         if (holders.isEmpty())
         {
 			trans.log("[Lock.isConflict] current Lock: " + this.lockType.typeToString() + " on account number " + account.getAccountNumber() + " does not conflict with " + type.typeToString());
@@ -95,7 +103,7 @@ public class Lock implements LockTypes
 			trans.log("[Lock.isConflict] current Lock: " + this.lockType.typeToString() + " on account number " + account.getAccountNumber() + " does not conflict with " + type.typeToString());
             return false;
         }
-        else if (this.lockType.getType() == WRITE_LOCK)
+        else if (this.lockType.getType() == WRITE_LOCK || this.lockType.getType() == WRITE_LOCK)
         {
             // if there is a write lock already, then theres a conflict.
 			trans.log("[Lock.isConflict] current Lock: " + this.lockType.typeToString() + " on account number " + account.getAccountNumber() + " conflicts with " + type.typeToString());
